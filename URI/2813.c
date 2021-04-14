b12ab37c4e214992f423d9c25ac78da1ae6a4a2f
Edson Isaac <65103163+Edssaac@users.noreply.github.com>
2021-04-14 20:21:22 -0300

    Add files via upload

--- URI/2813.c
+++ URI/2813.c
@@ -1,46 +1,43 @@
 #include <stdio.h>
 #include <string.h>
 
-int main() {
+int main()
+{
+    int casaComprado, casaSobrando, trabComprado, trabSobrando, dias;
+    char casa[10], trab[10];
 
-    int dias, C, E, casa, escritorio;
-    char SD[7], SN[7];
-    C=E=casa=escritorio=0;
+    casaComprado = casaSobrando = trabComprado = trabSobrando = 0;
+
 
     scanf("%d", &dias);
-
-    while (dias>0)
+    while(dias--)
     {
-        scanf("%s %s", SD, SN);
+        scanf("%s %s", casa, trab);
 
-        if ( strcmp(SD,"chuva")==0 && casa==0 )
+        if ( strcmp(casa, "chuva") == 0 && casaSobrando == 0 )
         {
-            C++;
-            escritorio=1;
+            casaComprado++;
+            trabSobrando++;
         }
-        else if ( strcmp(SD,"chuva")==0 && casa>0 )
+        else if ( strcmp(casa, "chuva") == 0&& casaSobrando > 0 )
         {
-            casa--;
-            escritorio++;
-        }
-
-
-        if ( strcmp(SN,"chuva")==0 && escritorio==0 )
-        {
-            E++;
-            casa++;
-        }
-        else if ( strcmp(SN,"chuva")==0 && escritorio>0 )
-        {
-            casa++;
-            escritorio--;
+            trabSobrando++;
+            casaSobrando--;
         }
 
-        dias--;
+         if ( strcmp(trab, "chuva") == 0 && trabSobrando == 0 )
+        {
+            trabComprado++;
+            casaSobrando++;
+        }
+        else if ( strcmp(trab, "chuva") == 0 && trabSobrando > 0 )
+        {
+            casaSobrando++;
+            trabSobrando--;
+        }
     }
 
-    printf("%d %d\n", C, E);
-
+    printf("%d %d\n", casaComprado, trabComprado);
 
     return 0;
 }
